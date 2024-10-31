b3eac41fdb65bf37bae119f1a7e40d9818104143
AbrarTunio <abrar.tunio@yahoo.com>
2024-10-31 20:18:49 +0500

    conditional operators

---
+++ 20241031/script.js
@@ -0,0 +1,18 @@
+// Data Type
+// string = "Something"
+// integer = 10.5
+// boolean = true
+
+
+// Conditional Operators
+// 1-  Equal to  == 
+// 2-  Not-equal to   !=
+// 3-  Strict Equal to  ===
+// 4-  greater than      >
+// 5-  greater than or equal to >=
+// 6-  less than                <
+// 7-  less than or equal to   <=
+
+let myName = true
+
+console.log( !myName )
